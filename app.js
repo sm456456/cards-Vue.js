c142517b83d3c1d108ff2e188e186d327a0ff665
Selma456 <74097172+sm456456@users.noreply.github.com>
2021-08-06 11:16:22 +0200

    add people

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ let vm = Vue.createApp({
       people: [
         {
           name: "Johny",
-          message: "I'd like to sell my car."
+          message: "I'm selling opel corsa from 2005."
+        },
+        {
+          name: "Sarah",
+          message:
+            "I would like to sell Ford fiesta in good condition from 2015."
+        },
+        {
+          name: "Natalia",
+          message: "Seat ibiza for sale. Just 2 years old."
+        },
+        {
+          name: "Timi",
+          message: "My favourite car so far; Volvo v40 from 2016. Buy it now!"
         }
       ]
     }
